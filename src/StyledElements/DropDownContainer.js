b9581c719e16d68e39d9984770effe9113f2ba19
AmirjanyanIT <amirjanyanit@gmail.com>
2020-10-10 17:09:44 +0400

    added scroll bar in dropdown

--- src/StyledElements/DropDownContainer.js
+++ src/StyledElements/DropDownContainer.js
@@ -3,11 +3,12 @@ import style from 'styled-components'
 export default style.div`
   position: absolute;
   justify-content: space-between;
-  min-height: 100px;
+  height: 80px;
   background-color: white;
   border: 1px solid #ececec;
   padding: 6px;
   border-radius: 6px;
   color: #8e8e8e;
   z-index: 9000;
+  overflow:auto;
 `
